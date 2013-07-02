b1433da05dc2b5a78f22089ecabc3f887f06356f
iuthisaam <iuthisaam@eww.(none)>
2013-07-02 11:55:37 +0500

    updates

--- rpc.js
+++ rpc.js
@@ -11,6 +11,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.compress());
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -24,7 +25,15 @@ app.configure('development', function(){
 });
 
 app.get('/', function(req,res){
-	c.tweets(function(t){
+	var cmd = req.query.cmd;
+	var arg = req.query.arg;
+	if(arg){
+		arg = decodeURIComponent(arg);
+	}
+	if(typeof c[cmd] == 'undefined'){
+		return res.json({error:"invalid command"});
+	}
+	c[cmd](arg, function(t){
 		res.json(t);
 	});
 });
